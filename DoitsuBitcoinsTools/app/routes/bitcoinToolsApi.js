173843677b8047094b43830875bf15cf25bc389d
DUCTHSE61994\thd11 <thd1152018@gmail.com>
2017-08-30 16:21:11 +0700

    update bitcoinToolsApi

--- DoitsuBitcoinsTools/app/routes/bitcoinToolsApi.js
+++ DoitsuBitcoinsTools/app/routes/bitcoinToolsApi.js
@@ -2,23 +2,23 @@ var bittrex = require('node.bittrex.api');
 var config = require('../../config.js');
 
 bittrex.options({
-	'apikey': config.apikey,
-	'apisecret': config.apisecret
+    'apikey': config.apikey,
+    'apisecret': config.apisecret
 });
 
 module.exports = function(app, express) {
-	var apiRouter = express.Router();
-	apiRouter.get('/marketNames', function(req, res) {
-		bittrex.getmarketsummaries(function(data, err) {
-			if (err) {
-				return console.error(err);
-			}
-			var outData = data.result.filter(m => m.MarketName.includes('BTC'));
-			res.send({
-				data: outData
-			});
-		})
-	});
+    var apiRouter = express.Router();
+    apiRouter.get('/marketNames', function(req, res) {
+        bittrex.getmarketsummaries(function(data, err) {
+            if (err) {
+                return console.error(err);
+            }
+            var outData = data.result.filter(m => m.MarketName.includes('BTC'));
+            res.send({
+                data: outData
+            });
+        })
+    });
 
-	return apiRouter;
+    return apiRouter;
 }
